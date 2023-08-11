3968d9b559cab46a20bb2f5101065ccb22cbdee0
Danny W <dannywang@utexas.edu>
2023-08-10 20:14:37 -0400

    fix file structure; add session package and set up server.js

--- main/controllers/index.js
+++ main/controllers/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-// const homeRoutes = require('./homeRoutes');
+const homeRoutes = require("./homeRoutes");
 
-// router.use('/', homeRoutes);
+router.use("/", homeRoutes);
 
 module.exports = router;
--- main/server.js
+++ main/server.js
@@ -3,12 +3,27 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
+const session = require("express-session");
+
 const sequelize = require("./config/connection");
+const sequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new sequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
