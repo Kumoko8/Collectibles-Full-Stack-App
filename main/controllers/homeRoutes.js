f8d16113acbaebb11c4188ac3cca54a53e71652d
Danny W <dannywang@utexas.edu>
2023-08-13 03:24:20 -0400

    add the log out function; add the auth helper file

--- main/controllers/homeRoutes.js
+++ main/controllers/homeRoutes.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
-  res.render("homepage");
+  res.render("homepage", { logged_in: req.session.logged_in });
 });
 
 // Login route
 router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
     res.redirect("/");
     return;
   }
