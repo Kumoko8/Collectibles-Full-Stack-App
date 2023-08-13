e60bd603adeea4641645ab5fddf81c484f1724d2
Danny W <dannywang@utexas.edu>
2023-08-13 03:01:01 -0400

    added login/logout basic html nad js to handle them; currently cannot find the login.js file for some reason.

--- main/controllers/homeRoutes.js
+++ main/controllers/homeRoutes.js
@@ -4,4 +4,13 @@ router.get("/", async (req, res) => {
   res.render("homepage");
 });
 
+// Login route
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
 module.exports = router;
